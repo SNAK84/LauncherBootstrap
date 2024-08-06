329d8593e68925a748237a0b5f8e10a879b8eace
SNAK84 <53576868+SNAK84@users.noreply.github.com>
2024-08-06 20:24:54 +0300

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static final Uri launcherJarUrl =
-      Uri.parse('https://your-project-site.ru/path/to/launcher.jar');
+      Uri.parse('https://mcl.snak84.ru/Launcher.jar');
 
-  static final String projectDirectoryName = 'your-project-dir-name';
+  static final String projectDirectoryName = '.SNAKMineCraft';
 }
